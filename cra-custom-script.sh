42cc58acafe651b2e8b64611eecb3865b1856553
Kaushik Patel <kaushikpatel@Mac.bbrouter>
2025-12-10 11:12:33 +0530

    cra custom script

--- cra-custom-script.sh
+++ cra-custom-script.sh
@@ -4,15 +4,20 @@ if [[ "${PIPELINE_DEBUG:-0}" == 1 ]]; then
     trap env EXIT
     env | sort
     set -x
+fi
 
+echo "[CRA Script] Installing dnf..."
 microdnf install -y dnf
-
 microdnf -y clean all
 
-microdnf remove -y python36 python36-libs python36-pip && microdnf clean all
+echo "[CRA Script] Removing Python 3.6..."
+microdnf remove -y python36 python36-libs python36-pip || true
+microdnf clean all
 
+echo "[CRA Script] Installing Python 3.11..."
 microdnf install -y python3.11 python3.11-pip
 
-python3 -m pip install --upgrade pip
+echo "[CRA Script] Upgrading pip..."
+python3.11 -m pip install --upgrade pip || python3 -m pip install --upgrade pip
 
 echo "Custom CRA Python setup script completed successfully."
